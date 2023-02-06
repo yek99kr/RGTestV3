264dbdf48e2742cc337f45c84535da3235fabccf
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-02-06 14:28:32 -0500

    bmw2023add

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import { linkResolver, repositoryName } from "../prismicio";
 import useMediaQueryHeight from "../utils/useMediaQueryHeight";
 import ShopProvider from "../context/shopContext";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import ContactPage from "../components/ContactPage";
 
 function MyApp({ Component, pageProps, router }) {
   const isSmallestHeight = useMediaQueryHeight(349);
@@ -36,26 +37,27 @@ function MyApp({ Component, pageProps, router }) {
               {!isSmallestHeight ? (
                 <Component {...pageProps} key={router.asPath} router={router} />
               ) : (
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[40%] w-[87vw] text-[3.1vw] sm:text-[16px] md:text-[17px] lg:text-[19px] xl:text-[21px] 2xl:text-[24.5px] text-center">
-                  <p className="font-[400] select-auto">
-                    Currently taking on select projects.{" "}
-                    <a
-                      href="mailto: hello@realgood.tv"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <span className="thumbcursor">
-                        <span>hello@realgood.tv</span>
+                <ContactPage />
+                // <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[40%] w-[87vw] text-[3.1vw] sm:text-[16px] md:text-[17px] lg:text-[19px] xl:text-[21px] 2xl:text-[24.5px] text-center">
+                //   <p className="font-[400] select-auto">
+                //     Currently taking on select projects.{" "}
+                //     <a
+                //       href="mailto: hello@realgood.tv"
+                //       target="_blank"
+                //       rel="noreferrer"
+                //     >
+                //       <span className="thumbcursor">
+                //         <span>hello@realgood.tv</span>
 
-                        <img
-                          src="/email.png"
-                          alt="email"
-                          className="w-[1.3em] inline m-2 noselect"
-                        />
-                      </span>
-                    </a>
-                  </p>
-                </div>
+                //         <img
+                //           src="/email.png"
+                //           alt="email"
+                //           className="w-[1.3em] inline m-2 noselect"
+                //         />
+                //       </span>
+                //     </a>
+                //   </p>
+                // </div>
               )}
             </AnimatePresence>
           </PrismicPreview>
