e62d0d7a706b22c174a9514a1f99e3a78a5ad2e3
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-02-20 18:47:09 -0500

    header

--- slices/DetailMedia/index.js
+++ slices/DetailMedia/index.js
@@ -9,31 +9,35 @@ const DetailMedia = ({ slice }) => {
         <>
           {slice.primary.media.link_type === "Web" ? (
             <>
-              <div className="col-start-3 md:p-0 w-[100vw] md:w-[100vw] aspect-video">
-                <iframe
-                  className="w-[100%] h-[100%]"
-                  src={`${slice.primary.media.url.slice(6)}?quality=1080p${
-                    slice.primary.autoplay
-                      ? "&autopause=0&loop=1&background=1&autoplay=1&muted=1&controls=0"
-                      : ""
-                  }`}
-                  allowFullScreen
-                  frameBorder="0"
-                ></iframe>
+              <div className="col-start-3 w-[100vw] aspect-video 2xl:overflow-hidden 2xl:aspect-[19/9]">
+                <div className="relative md:p-0 w-[100vw] md:w-[100vw] aspect-video 2xl:top-[-10%]">
+                  <iframe
+                    className="w-[100%] h-[100%]"
+                    src={`${slice.primary.media.url.slice(6)}?quality=1080p${
+                      slice.primary.autoplay
+                        ? "&autopause=0&loop=1&background=1&autoplay=1&muted=1&controls=0"
+                        : ""
+                    }`}
+                    allowFullScreen
+                    frameBorder="0"
+                  ></iframe>
+                </div>
               </div>
             </>
           ) : (
             slice.primary.media.url && (
               <>
-                <div className="relative col-start-3 md:p-0 w-[100vw] md:w-[100vw] aspect-video">
-                  <Image
-                    src={slice.primary.media.url}
-                    alt={slice.primary.media.alt}
-                    priority={true}
-                    loading="eager"
-                    layout="fill"
-                    objectFit="cover"
-                  ></Image>
+                <div className="col-start-3 w-[100vw] aspect-video  2xl:overflow-hidden 2xl:aspect-[19/9]">
+                  <div className="relative md:p-0 w-[100vw] md:w-[100vw] aspect-video 2xl:top-[-10%]">
+                    <Image
+                      src={slice.primary.media.url}
+                      alt={slice.primary.media.alt}
+                      priority={true}
+                      loading="eager"
+                      layout="fill"
+                      objectFit="cover"
+                    ></Image>
+                  </div>
                 </div>
               </>
             )
