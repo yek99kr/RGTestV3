ef720d9f25cb748b9a6388a6c065e21a64bb6924
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-07-11 19:33:31 -0400

    a

--- components/Project/ProjectList.js
+++ components/Project/ProjectList.js
@@ -9,27 +9,29 @@ const ProjectList = ({ projects }) => {
   const mouseAnimation = isSmallestHeight || isTouch;
 
   const classes = [
-    "md:mt-[2vw] md:rotate-[0deg] md:col-start-1 md:top-[-20px]",
-    "md:mt-[6vw] md:rotate-[-8deg] md:col-start-3",
-    "md:mt-[0vw] md:rotate-[9deg] md:col-start-5 ",
+    "md:mt-[2vw] md:rotate-[0deg] md:col-start-1 md:top-[-0px]",
+    "md:mt-[6vw] md:rotate-[-8deg] md:col-start-3 md:top-[20px]",
+    "md:mt-[0vw] md:rotate-[9deg] md:col-start-5 md:top-[-30px]",
 
-    "md:mt-[-5vw] md:rotate-[3deg] md:col-start-2 md:-ml-[13rem] md:top-[-20px]",
+    "md:mt-[-5vw] md:rotate-[3deg] md:col-start-2 md:-ml-[13rem] md:top-[-40px]",
     "md:mt-[-3vw] md:rotate-[-4deg] md:col-start-4 md:-mr-[10rem] md:top-[-30px]",
 
-    "md:mt-[0vw] md:rotate-[10deg] md:col-start-1 md:top-[-100px]",
-    "md:mt-[-3vw] md:rotate-[-2deg] md:col-start-3 md:top-[-50px]",
-    "md:mt-[-5vw] md:rotate-[2deg] md:col-start-5 md:top-[-50px]",
+    "md:mt-[0vw] md:rotate-[6deg] md:col-start-1 md:top-[-130px] md:-ml-[20px]",
+    "md:mt-[-3vw] md:rotate-[-2deg] md:col-start-3 xl:top-[80px] md:-ml-[-10px]",
+    "md:mt-[-5vw] md:rotate-[2deg] md:col-start-5 md:top-[-10px]",
 
     "md:mt-[0vw] md:rotate-[-3deg] md:col-start-2 md:-ml-[10rem] md:top-[-0px]",
-    "md:mt-[-3vw] md:rotate-[4deg] md:col-start-4 md:-mr-[12rem] md:top-[-0px] ",
+    "md:mt-[-3vw] md:rotate-[4deg] md:col-start-4 md:-mr-[12rem] xl:top-[100px] ",
 
-    "md:mt-[2vw] md:rotate-[10deg] md:col-start-1 md:top-[-100px] xl:top-[-110px] xl:-ml-[100px]",
-    "md:mt-[6vw] md:rotate-[-8deg] md:col-start-3  md:top-[150px] xl:top-[200px] md:-ml-[190px] xl:-ml-[390px] lg:pb-[100px]",
-    "md:mt-[0vw] md:rotate-[4deg] md:col-start-4 md:top-[-80px] xl:top-[-100px] xl:-ml-[50px]",
-    "md:mt-[0vw] md:rotate-[4deg] md:col-start-5 md:top-[110px] xl:top-[50px] md:-mr-[100px] xl:-mr-[250px]",
+    "md:mt-[2vw] md:rotate-[10deg] md:col-start-1 md:top-[-100px] xl:top-[-80px] xl:-ml-[100px]",
+    "md:mt-[6vw] md:rotate-[-8deg] md:col-start-3  md:top-[30px]  md:-ml-[0px] xl:-ml-[0px] lg:pb-[100px]",
+    "md:mt-[0vw] md:rotate-[4deg] md:col-start-5 md:top-[-80px] xl:top-[-60px] xl:-ml-[0px]",
+
+    "md:mt-[-3vw] md:rotate-[3deg] md:col-start-2 md:-ml-[13rem] md:top-[-0px]",
+    "md:mt-[-3vw] md:rotate-[-4deg] md:col-start-4 md:-mr-[10rem] md:top-[-30px]",
   ];
 
-  const sizes = [29, 26, 26, 28, 30, 24, 27, 25, 25, 26, 24, 25, 24, 30];
+  const sizes = [25, 29, 26, 28, 28, 25, 23, 24, 27, 25, 26, 26, 25, 29, 30];
 
   return (
     <div className="mx-auto py-16 px-5 sm:px-6 lg:px-0 sm:mt-[1rem] md:w-[90vw] lg:w-[80vw] xl:w-[75vw] ">
