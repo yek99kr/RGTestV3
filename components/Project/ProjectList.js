ecf5b9eafda439b30fe492501e5cb124e4dfff62
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-07-12 12:10:50 -0400

    aa

--- components/Project/ProjectList.js
+++ components/Project/ProjectList.js
@@ -9,7 +9,7 @@ const ProjectList = ({ projects }) => {
   const mouseAnimation = isSmallestHeight || isTouch;
 
   const classes = [
-    "md:mt-[2vw] md:rotate-[0deg] md:col-start-1 md:top-[-0px]",
+    "md:mt-[2vw] md:rotate-[-3deg] md:col-start-1 md:top-[-0px]",
     "md:mt-[6vw] md:rotate-[-8deg] md:col-start-3 md:top-[20px]",
     "md:mt-[0vw] md:rotate-[9deg] md:col-start-5 md:top-[-30px]",
 
@@ -31,7 +31,7 @@ const ProjectList = ({ projects }) => {
     "md:mt-[-3vw] md:rotate-[-4deg] md:col-start-4 md:-mr-[10rem] md:top-[-30px]",
   ];
 
-  const sizes = [25, 29, 26, 28, 28, 25, 23, 24, 27, 25, 26, 26, 25, 29, 30];
+  const sizes = [26, 29, 26, 28, 28, 25, 23, 24, 27, 25, 26, 26, 25, 29, 30];
 
   return (
     <div className="mx-auto py-16 px-5 sm:px-6 lg:px-0 sm:mt-[1rem] md:w-[90vw] lg:w-[80vw] xl:w-[75vw] ">
